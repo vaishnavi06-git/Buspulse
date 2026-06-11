7312d88e51bb7191550f19dbd09eb063f695ddea
vaishnavi06-git <golivaishnavi06@gmail.com>
2026-06-11 12:34:47 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "http://localhost:5000/api",
+  baseURL: "https://buspulse-backend.onrender.com/api",
 });
 
 export default api;
